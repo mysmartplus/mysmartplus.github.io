c8398ce6b87ba4c0f82863bd77cac5d2d5ed46ce
mysmartplus <140559922+mysmartplus@users.noreply.github.com>
2023-07-25 17:13:45 -0500

    mysmart plus
    mysmart plus

---
+++ index_files/simple-php-captcha.php
@@ -0,0 +1,4 @@
+<br />
+<b>Notice</b>:  Undefined index: _CAPTCHA in <b>/var/www/vhosts/lagruta.mx/httpdocs/captcha/simple-php-captcha.php</b> on line <b>127</b><br />
+<br />
+<b>Notice</b>:  Trying to access array offset on value of type null in <b>/var/www/vhosts/lagruta.mx/httpdocs/captcha/simple-php-captcha.php</b> on line <b>127</b><br />
